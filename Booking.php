c471fc8e5ac15f811f8f758e903c86d34a2fba7d
Hamza Odeh <dev@simboi.com>
2024-10-28 17:46:30 +0200

    Accept/reject lesson

--- Booking.php
+++ Booking.php
@@ -146,4 +146,146 @@
         $output = array('Result' => 'SUCCESS');
         return json_encode($output);
     }
+
+    /**
+     * Accept a lesson.
+     *
+     * @param Phone The teachers's phone number
+     * @param Password The teacher's password
+     * @param StudentID The student's ID
+     * @param StartTimestamp The start timestamp of the lesson
+     *
+     * @return JSON Object with the result of the operation
+     * @return Result=SUCCESS in case of success
+     * @return Result=LESSON_DOESNT_EXIST in case the lesson is not in the database
+     * @return Result=PHONE_DOESNT_EXIST in case the phone number is not in the database
+     * @return Result=ERROR in case of failure
+     */
+    function accept_lesson($conn, $phone, $password, $student_id, $start_timestamp)
+    {
+        $output = array('Result' => 'None');
+
+        // Check if the phone number is 12 characters long, if not, end the script
+        if (strlen($phone) != 12) die('{"Result": "ERROR"}');
+
+        // Check if the password is correct, and get the teacher's ID and CurrentAppointments
+        $sql = "SELECT ID, Password, CurrentAppointments FROM Teachers WHERE Phone = '".$phone."'";
+        $result = mysqli_query($conn ,$sql);
+        if(mysqli_num_rows($result) > 0) {
+            while($row = mysqli_fetch_assoc($result)) {
+                if (!password_verify($password, $row['Password'])) die('{"Result": "WRONG_PASSWORD"}');
+
+                $id = $row['ID'];
+                $teacher_current_appointments = json_decode($row['CurrentAppointments'], true);
+            }
+        } else {
+            die('{"Result": "PHONE_DOESNT_EXIST"}');
+        }
+
+        // Find the lesson to accept in the PendingLessons table
+        $sql = "SELECT Details FROM PendingLessons WHERE StudentID = '".$student_id."'";
+        $result = mysqli_query($conn ,$sql);
+        if(mysqli_num_rows($result) > 0) {
+            while($row = mysqli_fetch_assoc($result)) {
+                $details = json_decode($row['Details'], true);
+                if ($details['StartTimestamp'] == $start_timestamp) break;
+            }
+        } else {
+            die('{"Result": "LESSON_DOESNT_EXIST"}');
+        }
+
+        // Remove the lesson from the PendingLessons table
+        $sql = "DELETE FROM PendingLessons WHERE StudentID='".$student_id."' AND Details LIKE '%\"StartTimestamp\":".$start_timestamp."%'";
+        if (!mysqli_query($conn ,$sql)) die('{"Result": "ERROR"}');
+
+        $details['IsPending'] = false;
+
+        // Add the lesson to the teacher's CurrentAppointments
+        array_push($teacher_current_appointments, $details);
+        $sql = "UPDATE Teachers SET CurrentAppointments='".json_encode($teacher_current_appointments)."' WHERE Phone='".$phone."'";
+        if (!mysqli_query($conn ,$sql)) die('{"Result": "ERROR"}');
+
+        // Update the lesson in the user's CurrentAppointments
+        $sql = "SELECT CurrentAppointments FROM Customers WHERE ID = '".$student_id."'";
+        $result = mysqli_query($conn ,$sql);
+        if(mysqli_num_rows($result) > 0) {
+            while($row = mysqli_fetch_assoc($result)) {
+                $current_appointments = json_decode($row['CurrentAppointments'], true);
+
+                foreach ($current_appointments as $key => $appointment) {
+                    if ($appointment['StartTimestamp'] == $start_timestamp) {
+                        $current_appointments[$key] = $details;
+                        break;
+                    }
+                }
+
+                $sql = "UPDATE Customers SET CurrentAppointments='".json_encode($current_appointments)."' WHERE ID='".$student_id."'";
+                if (!mysqli_query($conn ,$sql)) die('{"Result": "ERROR"}');
+            }
+        } else {
+            die('{"Result": "ERROR"}');
+        }
+
+        $output = array('Result' => 'SUCCESS');
+        return json_encode($output);
+    }
+
+    /**
+     * Reject an accepted lesson. The lesson will be added back to the pending lessons.
+     *
+     * @param Phone The teachers's phone number
+     * @param Password The teacher's password
+     * @param StudentID The student's ID
+     * @param StartTimestamp The start timestamp of the lesson
+     *
+     * @return JSON Object with the result of the operation
+     * @return Result=SUCCESS in case of success
+     * @return Result=LESSON_DOESNT_EXIST in case the lesson is not in the database
+     * @return Result=PHONE_DOESNT_EXIST in case the phone number is not in the database
+     * @return Result=ERROR in case of failure
+     */
+    function reject_lesson($conn, $phone, $password, $student_id, $start_timestamp)
+    {
+        $output = array('Result' => 'None');
+
+        // Check if the phone number is 12 characters long, if not, end the script
+        if (strlen($phone) != 12) die('{"Result": "ERROR"}');
+
+        // Check if the password is correct, and get the teacher's ID and CurrentAppointments
+        $sql = "SELECT ID, Password, CurrentAppointments FROM Teachers WHERE Phone = '".$phone."'";
+        $result = mysqli_query($conn ,$sql);
+        if(mysqli_num_rows($result) > 0) {
+            while($row = mysqli_fetch_assoc($result)) {
+                if (!password_verify($password, $row['Password'])) die('{"Result": "WRONG_PASSWORD"}');
+
+                $id = $row['ID'];
+                $teacher_current_appointments = json_decode($row['CurrentAppointments'], true);
+            }
+        } else {
+            die('{"Result": "PHONE_DOESNT_EXIST"}');
+        }
+
+        // Find the lesson to reject in the teacher's CurrentAppointments
+        $found = false;
+        foreach ($teacher_current_appointments as $key => $appointment) {
+            if ($appointment['StudentID'] == $student_id && $appointment['StartTimestamp'] == $start_timestamp) {
+                $found = true;
+                $details = $appointment;
+                $details['IsPending'] = true;
+                unset($teacher_current_appointments[$key]);
+            }
+        }
+        if (!$found) die('{"Result": "LESSON_DOESNT_EXIST"}');
+
+        // Remove the lesson from the teacher's CurrentAppointments
+        $sql = "UPDATE Teachers SET CurrentAppointments='".json_encode(array_values($teacher_current_appointments))."' WHERE Phone='".$phone."'";
+        if (!mysqli_query($conn ,$sql)) die('{"Result": "ERROR"}');
+
+        // Add the lesson back to the PendingLessons table
+        $sql = "INSERT INTO PendingLessons (StudentID, Details) VALUES ('".$student_id."', '".json_encode($details)."')";
+        if (!mysqli_query($conn, $sql)) die('{"Result": "ERROR"}');
+
+        $output = array('Result' => 'SUCCESS');
+        return json_encode($output);
+    }
 ?>
